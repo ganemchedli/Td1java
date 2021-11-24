c6fb5a98b14757f122d7c43f38c0f38cecd00b1e
Ganem Chedli <ganemchedli@gmail.com>
2021-11-24 17:43:31 +0100

    Question 2 in exercice 3 is done

--- src/Exercice3/Question2.java
+++ src/Exercice3/Question2.java
@@ -2,12 +2,19 @@ package Exercice3;
 import java.util.*;
 
 public class Question2 {
-   public static void main(String args[]) {
+   private static String newStr;
+
+public static void main(String args[]) {
 	   Scanner myObj=new Scanner(System.in);
 	   String str = myObj.nextLine() ;
-	   char c = myObj.next.charAt(0);
+	   char c = myObj.next().charAt(0);
 	   for(int i = 0 ; i<str.length() ; i++) {
-		   
+		   char cStr = str.charAt(i) ; 
+		   if(cStr == c ) {
+			   newStr = str.substring(0,i)+' '+str.substring(i+1);
+			   break ; 
+		   }
 	   }
+	   System.out.println(newStr);
    }
 }
